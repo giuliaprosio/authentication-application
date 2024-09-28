34088e2e70e02ffb02cd9dffedeaac1347e7c227
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2024-09-28 15:52:02 +0200

    disable cors

--- src/main/java/com/springapplication/userapp/config/WebSecurityConfig.java
+++ src/main/java/com/springapplication/userapp/config/WebSecurityConfig.java
@@ -9,10 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -26,6 +28,11 @@ public class WebSecurityConfig {
         authProvider.setPasswordEncoder(passwordEncoder);
 
         return authProvider;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
 
     @Bean
