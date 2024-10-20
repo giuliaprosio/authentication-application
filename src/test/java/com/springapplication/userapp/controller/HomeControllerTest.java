bc5ed596b35a9c0cce6c37715385d10de99ba6d8
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2024-10-20 16:49:24 +0200

    Test protected endpoint controller

---
+++ src/test/java/com/springapplication/userapp/controller/HomeControllerTest.java
@@ -0,0 +1,54 @@
+package com.springapplication.userapp.controller;
+
+import com.c4_soft.springaddons.security.oauth2.test.annotations.WithMockJwtAuth;
+import com.springapplication.userapp.config.CustomAuthenticationSuccessHandler;
+import com.springapplication.userapp.service.UserDetailsServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest(controllers = {HomeController.class})
+@Import(HomeController.class)
+public class HomeControllerTest {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
+
+    @MockBean
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+
+    private static final String ENDPOINT = "/home";
+
+    @Test
+    @WithMockJwtAuth
+    public void GetHome_ReturnHome() throws Exception {
+
+        MvcResult result = this.mockMvc
+                .perform(get(ENDPOINT))
+                .andExpect(status().isOk())
+                .andExpect(content().string("welcome home"))
+                .andReturn();
+    }
+
+    @Test
+    public void GetHomeUnauthorized_Fails() throws Exception {
+
+        MvcResult result = this.mockMvc
+                .perform(get(ENDPOINT))
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+    }
+
+}
